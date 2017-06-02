cf46c05976c636926ea0abbf47bd57b1c5000e4b
Esvethlynna Pangelinan <esveth@aol.com>
2017-06-01 20:53:33 -0700

    add code to remove previously displayed image and animal info

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
   $("#animal").change(function() {
     var animalSelect = parseInt($("#animal").val());
-    alert(animalSelect);
+  //  alert(animalSelect);
 
     if (animalSelect === 1) {
         $('.whales').show();
+        $('.turtles').hide();
+        $('.snakes').hide();
       } else if (animalSelect  === 2) {
         $('.turtles').show();
+        $('.whales').hide();
+        $('.snakes').hide();
       } else if (animalSelect === 3){
         $('.snakes').show();
+        $('.turtles').hide();
+        $('.whales').hide();
       } else {
         alert("Please select an animal");
       }
